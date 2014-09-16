95ab193e9f4d077637e90c2514ac360a671594ec
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-09-16 12:47:20 +0200

    More tests

--- tests/unittest/basics.py
+++ tests/unittest/basics.py
@@ -14,10 +14,22 @@ class BaseTest(unittest.TestCase):
         self.ib = DummyInfoBroker()
         self.sc = DummyServiceComposer()
         self.ch = DummyCloudHandler()
-        self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
     def test_cmd_1(self):
+        self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
         self.assertEqual(self.infrap.cri_create_env(None).__class__,
                          ip.InfraProcessor.CreateEnvironment)
+    def test_cmd_2(self):
+        self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
+        self.assertEqual(self.infrap.cri_create_node(None).__class__,
+                         ip.InfraProcessor.CreateNode)
+    def test_cmd_3(self):
+        self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
+        self.assertEqual(self.infrap.cri_drop_environment(None).__class__,
+                         ip.InfraProcessor.DropEnvironment)
+    def test_cmd_4(self):
+        self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
+        self.assertEqual(self.infrap.cri_drop_node(None).__class__,
+                         ip.InfraProcessor.DropNode)
 
 if __name__ == '__main__':
     unittest.main()
