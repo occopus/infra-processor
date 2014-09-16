f7374f40573c5dd4807f92c16cc656c0d4f15ef2
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-09-16 12:50:27 +0200

    More basic tests

--- tests/unittest/basics.py
+++ tests/unittest/basics.py
@@ -30,6 +30,22 @@ class BaseTest(unittest.TestCase):
         self.infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
         self.assertEqual(self.infrap.cri_drop_node(None).__class__,
                          ip.InfraProcessor.DropNode)
+    def test_remote_cmd_1(self):
+        self.infrap = ip.RemoteInfraProcessor(None)
+        self.assertEqual(self.infrap.cri_create_env(None).__class__,
+                         ip.InfraProcessor.CreateEnvironment)
+    def test_remote_cmd_2(self):
+        self.infrap = ip.RemoteInfraProcessor(None)
+        self.assertEqual(self.infrap.cri_create_node(None).__class__,
+                         ip.InfraProcessor.CreateNode)
+    def test_remote_cmd_3(self):
+        self.infrap = ip.RemoteInfraProcessor(None)
+        self.assertEqual(self.infrap.cri_drop_environment(None).__class__,
+                         ip.InfraProcessor.DropEnvironment)
+    def test_remote_cmd_4(self):
+        self.infrap = ip.RemoteInfraProcessor(None)
+        self.assertEqual(self.infrap.cri_drop_node(None).__class__,
+                         ip.InfraProcessor.DropNode)
 
 if __name__ == '__main__':
     unittest.main()
