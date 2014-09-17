1f5731a497e79cd7acc1242df2a1fe716473cb34
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-09-17 09:34:40 +0200

    Create bootstrap tests

--- tests/unittest/common.py
+++ tests/unittest/common.py
@@ -15,6 +15,23 @@ logging.config.dictConfig(cfg.logging)
 
 log = logging.getLogger('occo.unittests')
 
+class DummyNode(object):
+    def __init__(self, id, environment_id):
+        self._id = id
+        self._environment_id = environment_id
+        self._started = False
+    @property
+    def id(self):
+        return self._id
+    @property
+    def environment_id(self):
+        return self._environment_id
+    @property
+    def started(self):
+        return self._started
+    def __repr__(self):
+        return '(%s_%r)'%(self.id, self.started)
+
 class DummyInfoBroker(object):
     def __init__(self):
         self.environments = dict()
@@ -22,12 +39,10 @@ class DummyInfoBroker(object):
     def get(key):
         pass
     def __repr__(self):
-        def nodelist_repr(nodelist):
-            repr_list = ('%s:%r'%(n[0].id, n[1])
-                         for n in nodelist)
-            return ', '.join(repr_list)
-        envlist_repr = ('%s:[%s]'%(k, nodelist_repr(v))
-                        for (k, v) in self.environments.iteritems())
+        log.info('%r', self.environments)
+        nodelist_repr = lambda nodelist: ', '.join(repr(n) for n in nodelist)
+        envlist_repr = list('%s:[%s]'%(k, nodelist_repr(v))
+                            for (k, v) in self.environments.iteritems())
         return ' '.join(envlist_repr)
 
 class DummyServiceComposer(object):
@@ -35,14 +50,16 @@ class DummyServiceComposer(object):
         self.ib = infobroker
     def register_node(self, node):
         log.debug("[SC] Registering node: %r", node)
-        self.ib.environments[node.environment].append(node)
-        self.ib.node_lookup[node.id] = (node.environment, False)
+        self.ib.environments[node.environment_id].append(node)
+        self.ib.node_lookup[node.id] = node
         log.debug("[SC] Done - '%r'", self.ib)
     def drop_node(self, node_id):
         log.debug("[SC] Dropping node '%s'", node_id)
-        env_id = self.ib.node_lookup[node_id]
+        node = self.ib.node_lookup[node_id]
+        env_id = node.environment_id
         self.ib.environments[env_id] = list(
-            i for i in self.ib.environments[env_id] if i.id != node_id)
+            i for i in self.ib.environments[env_id]
+            if i.id != node_id)
         del self.ib.node_lookup[node_id]
         log.debug("[SC] Done - '%r'", self.ib)
 
@@ -60,5 +77,11 @@ class DummyCloudHandler(object):
         self.ib = infobroker
     def create_node(self, node):
         log.debug("[CH] Creating node: %r", node)
+        node._started = True
+        log.debug("[SC] Done - '%r'", self.ib)
+
     def drop_node(self, node_id):
         log.debug("[CH] Dropping node '%s'", node_id)
+        node = self.ib.node_lookup[node_id]
+        node._started = False
+        log.debug("[SC] Done - '%r'", self.ib)
