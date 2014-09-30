de6d7de8a36d2423f43a62e65a61adaecb476e9c
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2014-09-30 14:45:23 +0200

    Threadify remote tests

--- tests/unittest/remote.py
+++ tests/unittest/remote.py
@@ -7,7 +7,8 @@
 import unittest
 from common import *
 import occo.infraprocessor as ip
-import threading
+import threading as th
+import time
 
 class BaseTest(unittest.TestCase):
     def setUp(self):
@@ -15,17 +16,26 @@ class BaseTest(unittest.TestCase):
         self.sc = DummyServiceComposer(self.ib)
         self.ch = DummyCloudHandler(self.ib)
     def test_create_environment(self):
+        event = th.Event()
         infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
         skeleton = ip.RemoteInfraProcessorSkeleton(
-            infrap, cfg.ip_mqconfig, cfg.ctl_mqconfig, None)
+            infrap, cfg.ip_mqconfig, cfg.ctl_mqconfig, event)
         stub = ip.RemoteInfraProcessor(cfg.ip_mqconfig)
 
         eid = uid()
         cmd = stub.cri_create_env(eid)
         with stub:
             stub.push_instructions(cmd)
-        with skeleton:
-            skeleton.start_consuming()
+        def cth():
+            try:
+                with skeleton:
+                    skeleton.start_consuming()
+            except Exception:
+                log.exception('Exception in consumer thread')
+        consumer = th.Thread(target=cth)
+        consumer.start()
+        time.sleep(1)
+        event.set()
         self.assertEqual(repr(self.ib), '%s:[]'%eid)
 #    def test_create_node(self):
 #        infrap = ip.InfraProcessor(self.ib, self.ch, self.sc)
