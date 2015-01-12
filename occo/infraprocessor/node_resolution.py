f3717b6ffd039ccf2d2371f6036d28f2be650068
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-01-12 16:04:31 +0100

    Fixup node resolution to use the right data
    There's an additional level of indirection which must be resolved before
    resolving anything else.
    (node_description['type'] -> node_type_definition)

--- occo/infraprocessor/node_resolution.py
+++ occo/infraprocessor/node_resolution.py
@@ -12,39 +12,48 @@ import yaml
 log = logging.getLogger('occo.infraprocessor.node_resolution')
 
 def resolve_node(ib, node_id, node_description):
-    resolver = Resolver(node_description['implementation_type'],
-                        ib, node_id, node_description)
-    return resolver.resolve_node()
+    # Resolve node definition
+        # TODO: Alternative, future version:
+        # node_definition = brokering_service.select_implementation(
+        #                                node['type'], node.get('backend_id'))
+        # The brokering servie will call ib.get() as necessary.
+    node_definition = ib.get('node.definition',
+                             node_description['type'],
+                             node_description.get('backend_id'))
+    resolver = Resolver(protocol=node_definition['implementation_type'],
+                        info_broker=ib,
+                        node_id=node_id,
+                        node_description=node_description)
+    resolver.resolve_node(node_definition)
+    return node_definition
 
 class Resolver(factory.MultiBackend):
     def __init__(self, protocol, info_broker, node_id, node_description):
         self.info_broker = info_broker
         self.node_id = node_id
         self.node_description = node_description
-    def resolve_node(self):
-        raise NotImplementedError()
+    def resolve_node(self, node_definition):
+        """
+        In a sub-class, node_definition should be updated in-place with
+        the necessary information.
+        node_definition is also returned for convenience.
+        """
+        return node_definition
 
 @factory.register(Resolver, 'chef+cloudinit')
 class ChefCloudinitResolver(Resolver):
-    def resolve_node(self):
+    def resolve_node(self, node_definition):
         # Shorthands
         node = self.node_description
         ib = self.info_broker
         node_id = self.node_id
 
-        # Resolve node definition
-        resolved_node = ib.get('node.definition',
-                               node['type'], node.get('backend_id'))
-        # TODO: Alternative, future version:
-        # resolved_node = brokering_service.select_implementation(
-        #                                node['type'], node.get('backend_id'))
-        # The brokering servie will call ib.get() as necessary.
-
         # Amend resolved node with basic information
-        resolved_node['id'] = node_id
-        resolved_node['name'] = node['name']
-        resolved_node['environment_id'] = node['environment_id']
+        node_definition['id'] = node_id
+        node_definition['name'] = node['name']
+        node_definition['environment_id'] = node['environment_id']
         # Resolve backend-specific authentication information
-        resolved_node['auth_data'] = ib.get('backends.auth_data',
-                                            resolved_node['backend_id'],
-                                            node['user_id'])
+        node_definition['auth_data'] = ib.get('backends.auth_data',
+                                              node_definition['backend_id'],
+                                              node['user_id'])
+        return node_definition
