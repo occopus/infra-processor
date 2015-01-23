03c9b61e7a6d5da7e1b9f3bfd8ac145307ed743a
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-01-23 14:31:42 +0100

    Document occo.infraprocessor.node_resolution

--- occo/infraprocessor/node_resolution.py
+++ occo/infraprocessor/node_resolution.py
@@ -2,7 +2,26 @@
 # Copyright (C) 2014 MTA SZTAKI
 #
 
-__all__ = ['resolve_node']
+""" Resolution of node definitions.
+
+.. moduleauthor:: Adam Visegradi <adam.visegradi@sztaki.mta.hu>
+
+This module provides utilities to resolve the definition of an infrastructure
+node. That is, it uses the abstract definition of the node's type (its
+:ref:`implementation <nodedefinition>`) and gathers all the information
+necessary to actually create and build such a node on a given backend.
+
+The correct resolution algorithm **depends on** the type of description, which
+is a function of the type of the **service composer** and the type of the
+**cloud handler** used to instantiate the node. Because of this dependency,
+the resolution utilises the :mod:`Factory <occo.util.factory>` pattern to
+select the correct :class:`Resolver`.
+
+.. autoclass:: ChefCloudinitResolver
+    :members:
+"""
+
+__all__ = ['resolve_node', 'Resolver']
 
 import logging
 import occo.util as util
@@ -13,11 +32,24 @@ import jinja2
 log = logging.getLogger('occo.infraprocessor.node_resolution')
 
 def resolve_node(ib, node_id, node_description):
-    # Resolve node definition
-        # TODO: Alternative, future version:
-        # node_definition = brokering_service.select_implementation(
-        #                                node['type'], node.get('backend_id'))
-        # The brokering servie will call ib.get() as necessary.
+    """
+    Resolve node definition
+
+    :param str node_id: The internal unique identifier of the node.
+    :param node_description: The description of the node type. This will be
+        resolved to :ref:`Node Definition <nodedefinition>`, which will then
+        be resolved, filled in with information, to get a
+        :ref:`Resolved Node Definition <resolvednode>`.
+    :type node_description: :ref:`Node Description <nodedescription>`
+
+    .. todo:: Alternative, future version:
+
+        .. code:: python
+
+            node_definition = brokering_service.select_implementation(
+                                           node['type'], node.get('backend_id'))
+            # The brokering service will call ib.get() as necessary.
+    """
     node_definition = ib.get('node.definition',
                              node_description['type'],
                              node_description.get('backend_id'))
@@ -29,28 +61,71 @@ def resolve_node(ib, node_id, node_description):
     return node_definition
 
 class Resolver(factory.MultiBackend):
+    """
+    Abstract interface for node resolution.
+
+    This is an :class:`abstract factory <occo.util.factory.factory>` class.
+
+    :param protocol: The key identifying the actual resolver class.
+
+    :param info_broker: A reference to an information provider.
+    :type info_broker: :class:`~occo.infobroker.provider.InfoProvider`
+
+    :param str node_id: The internal identifier of the node instance.
+
+    :param node_description: The original node description.
+    :type node_description: :ref:`Node Description <nodedescription>`
+    """
     def __init__(self, protocol, info_broker, node_id, node_description):
         self.info_broker = info_broker
         self.node_id = node_id
         self.node_description = node_description
     def resolve_node(self, node_definition):
         """
-        In a sub-class, node_definition should be updated in-place with
-        the necessary information.
-        node_definition is also returned for convenience.
-        The resulting node definition must contain all information intended for
-        both the service composer and the cloud handler.
+        Overriden in a sub-class, ``node_definition`` should be updated
+        in-place with the necessary information. ``node_definition`` is also
+        returned for convenience. The resulting :ref:`resolved node definition
+        <resolvednode>` must contain all information intended for both the
+        service composer and the cloud handler.
+
+        :param node_definition: The definition that needs to be resolved.
+            The definition will be updated in place.
+        :type node_definition: :ref:`Node Definition <nodedefinition>`
+
+        :return: A reference to ``node_definition``.
+        :rtype: :ref:`Resolved Node Definition <resolvednode>`
         """
         return node_definition
 
 @factory.register(Resolver, 'chef+cloudinit')
 class ChefCloudinitResolver(Resolver):
+    """
+    Implementation of :class:`Resolver` for implementations for `cloud-init`_
+    with Chef_.
+
+    This resolver updates the node definition with infromation required by the
+    Chef :ref:`Service Composer <servicecomposer>` and any kind of :ref:`Cloud
+    Handler <cloudhandler>` that uses cloud-init; for example the
+    :class:`~occo.cloudhandler.backend.boto.BotoCloudHandler`.
+
+    .. todo:: This aspect of OCCO (resolving) must be thought over.
+
+    .. _`cloud-init`: https://cloudinit.readthedocs.org/en/latest/
+    .. _Chef: https://www.chef.io/
+    """
     def resolve_node(self, node_definition):
+        """
+        Implementation of :meth:`Resolver.resolve_node`.
+        """
+
         # Shorthands
         node = self.node_description
         ib = self.info_broker
         node_id = self.node_id
 
+        # TODO: the context depends on the sc AND the ch too.
+        # The same service composer may require different contexts for
+        # different backends.
         sc_data = ib.get('service_composer.aux_data',
                          node_definition['service_composer_id'])
         template = jinja2.Template(sc_data['context_template'])
