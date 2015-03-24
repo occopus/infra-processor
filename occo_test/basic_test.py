5ad3a7eb5bea2a21671509bcea7bd823644ed031
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-24 11:35:10 +0100

    Avoid implicitly calling __init__ with protocol specified

--- occo_test/basic_test.py
+++ occo_test/basic_test.py
@@ -10,6 +10,7 @@ import occo.infraprocessor as ip
 import occo.util as util
 from occo.infobroker.uds import UDS
 from occo.infobroker.kvstore import KeyValueStore
+from occo.util.factory import instantiate
 import threading
 
 class Stuff(): pass
@@ -26,33 +27,40 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(self.infrap.cri_create_env(Stuff()).__class__,
                          ip.CreateEnvironment)
     def test_cmd_2(self):
-        self.infrap = ip.InfraProcessor(self.uds, self.ch, self.sc,
-                                        protocol='basic')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  self.uds, self.ch, self.sc,
+                                  protocol='basic')
         self.assertEqual(self.infrap.cri_create_node(Stuff()).__class__,
                          ip.CreateNode)
     def test_cmd_3(self):
-        self.infrap = ip.InfraProcessor(self.uds, self.ch, self.sc,
-                                        protocol='basic')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  self.uds, self.ch, self.sc,
+                                  protocol='basic')
         self.assertEqual(self.infrap.cri_drop_environment(Stuff()).__class__,
                          ip.DropEnvironment)
     def test_cmd_4(self):
-        self.infrap = ip.InfraProcessor(self.uds, self.ch, self.sc,
-                                        protocol='basic')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  self.uds, self.ch, self.sc,
+                                  protocol='basic')
         self.assertEqual(self.infrap.cri_drop_node(Stuff()).__class__,
                          ip.DropNode)
     def test_remote_cmd_1(self):
-        self.infrap = ip.InfraProcessor(cfg.ip_mqconfig, protocol='remote')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  cfg.ip_mqconfig, protocol='remote')
         self.assertEqual(self.infrap.cri_create_env(Stuff()).__class__,
                          ip.CreateEnvironment)
     def test_remote_cmd_2(self):
-        self.infrap = ip.InfraProcessor(cfg.ip_mqconfig, protocol='remote')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  cfg.ip_mqconfig, protocol='remote')
         self.assertEqual(self.infrap.cri_create_node(Stuff()).__class__,
                          ip.CreateNode)
     def test_remote_cmd_3(self):
-        self.infrap = ip.InfraProcessor(cfg.ip_mqconfig, protocol='remote')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  cfg.ip_mqconfig, protocol='remote')
         self.assertEqual(self.infrap.cri_drop_environment(Stuff()).__class__,
                          ip.DropEnvironment)
     def test_remote_cmd_4(self):
-        self.infrap = ip.InfraProcessor(cfg.ip_mqconfig, protocol='remote')
+        self.infrap = instantiate(ip.InfraProcessor,
+                                  cfg.ip_mqconfig, protocol='remote')
         self.assertEqual(self.infrap.cri_drop_node(Stuff()).__class__,
                          ip.DropNode)
