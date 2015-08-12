515121586801ef513fe812c3323bc07a5bf6bdf3
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-08-12 14:59:48 +0000

    Tidying, commenting, documenting

--- occo/infraprocessor/node_resolution/backends/chef.py
+++ occo/infraprocessor/node_resolution/backends/chef.py
@@ -4,6 +4,8 @@
 
 """ Chef resolver for node definitions.
 
+.. _`connect`: https://gitlab.lpds.sztaki.hu/cloud-orchestrator/connect-cookbook
+
 .. moduleauthor:: Adam Visegradi <adam.visegradi@sztaki.mta.hu>
 
 """
@@ -16,6 +18,7 @@ import logging
 import occo.util as util
 import occo.exceptions as exceptions
 import occo.util.factory as factory
+import sys
 import yaml
 import jinja2
 from ..resolution import Resolver
@@ -80,6 +83,10 @@ class ChefCloudinitResolver(Resolver):
             return attrs
 
     def attr_connect_resolve(self, node, attrs, attr_mapping):
+        """
+        Transform connection specifications into an attribute that the cookbook
+        `connect`_ can understand.
+        """
         connections = [
             dict(source_role="{0}_{1}".format(node['infra_id'], role),
                  source_attribute=mapping['attributes'][0],
@@ -91,6 +98,12 @@ class ChefCloudinitResolver(Resolver):
         attrs['connections'] = connections
 
     def resolve_attributes(self, node_desc, node_definition, template_data):
+        """
+        Resolve the attributes of a node:
+        - Merge attributes of the node desc. and node def. (node desc overrides)
+        - Resolve string attributes as Jinja templates
+        - Construct an attribute to connect nodes
+        """
         attrs = node_definition.get('attributes', dict())
         attrs.update(node_desc.get('attributes', dict()))
         attr_mapping = node_desc.get('mappings', dict()).get('inbound', dict())
@@ -106,6 +119,9 @@ class ChefCloudinitResolver(Resolver):
 
         .. todo:: Maybe this should be moved to the Compiler. The IP
             depends on it, not the Chef service-composer.
+
+        .. todo:: Furthermore, synch_attrs will be obsoleted, and moved to
+            basic synch_strategy as parameters.
         """
         outedges = node_desc.get('mappings', dict()).get('outbound', dict())
 
@@ -114,9 +130,18 @@ class ChefCloudinitResolver(Resolver):
                 if mapping['synch']]
 
     def assemble_template_data(self, node_desc, node_definition):
+        """
+        Create the data structure that can be used in the Jinja templates.
+
+        .. todo:: Document the possibilities.
+        """
         from occo.infobroker import main_info_broker
 
         def find_node_id(node_name):
+            """
+            Convenience function to be used in templates, to acquire a node id
+            based on node name.
+            """
             nodes = main_info_broker.get(
                 'node.find', infra_id=node_desc['infra_id'], name=node_name)
             if not nodes:
@@ -133,7 +158,7 @@ class ChefCloudinitResolver(Resolver):
         # As it is used only for rendering a template, it is yet read-only.
         # If, for any reason, something starts modifying it, dict.update()-s
         # will have to be changed to deep copy to avoid side effects. (Just
-        # like in compiler, when node variables are assembled.)
+        # like in Compiler, when node variables are assembled.)
         source_data = dict(node_id=self.node_id)
         source_data.update(node_desc)
         source_data.update(node_definition)
@@ -142,23 +167,38 @@ class ChefCloudinitResolver(Resolver):
         return source_data
 
     def check_if_cloud_config(self, node_definition):
+        """
+        Process context iff it's a cloud-config.
+        """
+        # TODO: Maybe this condition can be imporoved? Need to verify.
         if not node_definition['context'].startswith('#cloud-config'):
             return
+
+        # Verify that the context *is* parsable by YAML. Otherwise, cloud-init
+        # will fail silently.
         try:
             yaml.load(node_definition['context'])
         except yaml.YAMLError as e:
             if hasattr(e, 'problem_mark'):
                 msg=('Schema error in context of '
-                     'node definition at line {0}.').format( e.problem_mark.line)
+                     'node definition at line {0}.').format(e.problem_mark.line)
             else:
                 msg='Schema error in context of node definition.'
-            raise exceptions.NodeContextSchemaError(
-                    node_definition=node_definition, reason=e, msg=msg)
+
+            raise \
+                exceptions.NodeContextSchemaError(
+                    node_definition=node_definition, reason=e, msg=msg), \
+                None, sys.exc_info()[2]
 
     def check_template(self, node_definition):
-        """Checks the context part of node definition"""
+        """
+        Checks the context part of node definition.
+
+        :raises occo.exceptions.orchestration.NodeContextSchemaError: if the
+            contextualization is invalid.
+        """
         self.check_if_cloud_config(node_definition)
-        pass
+        # Checks for other types of contextualization can be listed here
 
     def render_template(self, node_definition, template_data):
         """Renders the template pertaining to the node definition"""
@@ -171,6 +211,7 @@ class ChefCloudinitResolver(Resolver):
         """
 
         # Shorthands
+        nd = node_definition
         node_desc = self.node_description
         ib = self.info_broker
         node_id = self.node_id
@@ -180,13 +221,14 @@ class ChefCloudinitResolver(Resolver):
         node_definition['node_id'] = node_id
         node_definition['name'] = node_desc['name']
         node_definition['infra_id'] = node_desc['infra_id']
-        node_definition['auth_data'] = ib.get('backends.auth_data',
-                                              node_definition['backend_id'],
-                                              node_desc['user_id'])
-        node_definition['context'] = self.render_template(node_definition,
-                                                          template_data)
-        node_definition['attributes'] = self.resolve_attributes(
-            node_desc, node_definition, template_data)
+        node_definition['auth_data'] = \
+            ib.get('backends.auth_data',
+                   node_definition['backend_id'],
+                   node_desc['user_id'])
+        node_definition['context'] = \
+            self.render_template(node_definition, template_data)
+        node_definition['attributes'] = \
+            self.resolve_attributes(node_desc, node_definition, template_data)
         node_definition['synch_attrs'] = \
             self.extract_synch_attrs(node_desc)
 
